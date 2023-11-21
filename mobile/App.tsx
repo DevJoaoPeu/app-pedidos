c47550a59877b6c0b0152befe5f4acadc286efab
DevJoaoPeu <joaocanaldroid@gmail.com>
2023-11-21 11:36:54 -0300

    mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <View> 
+      <Text>Teste</Text>
+      <StatusBar backgroundColor="#1d1d2e" barStyle="light-content" translucent={false} />
+    </View>
+  );
+}
+
