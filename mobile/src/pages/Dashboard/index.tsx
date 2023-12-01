2f8d6c1c180749ab4e8854a2ccae264c20c56bac
DevJoaoPeu <joaocanaldroid@gmail.com>
2023-12-01 09:18:03 -0300

    feat: Pagina dashboard

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,10 +1,72 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useContext } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import { AuthContext } from "../../context/AuthContext";
 
-export default function Dashboard(){
-    return (
-        <View>
-            <Text>Tela Dashboard</Text>
-        </View>
-    )
+export default function Dashboard() {
+  const { signOut } = useContext(AuthContext);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Novo pedido</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Numero da mesa"
+        placeholderTextColor="#f0f0f0"
+        keyboardType="numeric"
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Abrir mesa</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 15,
+    backgroundColor: "#1d1d2e",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 24,
+  },
+  input: {
+    width: "90%",
+    height: 50,
+    backgroundColor: "#101026",
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    textAlign: "center",
+    fontSize: 22,
+    color: "#fff",
+    marginBottom: 7
+  },
+  button: {
+    width: "90%",
+    height: 40,
+    backgroundColor: "#3fffa3",
+    borderRadius: 4,
+    marginVertical: 12,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#101026",
+    fontWeight: "bold",
+  },
+});
