62dbd44c507ec1659e53d05b32ee8ea9fe62e688
DevJoaoPeu <joaocanaldroid@gmail.com>
2023-12-11 11:30:39 -0300

    feat: excluindo item da minha lista

--- mobile/src/components/ListItem/index.tsx
+++ mobile/src/components/ListItem/index.tsx
@@ -9,16 +9,21 @@ interface ItemProps {
     name: string;
     amount: string | number;
   };
+  deleteItem: (item_id: string) => void;
 }
 
-export function ListItem({ data }: ItemProps) {
+export function ListItem({ data, deleteItem }: ItemProps) {
+  function handleDeleteItem() {
+    deleteItem(data.id);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.item}>
         {data.amount} - {data.name}
       </Text>
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleDeleteItem}>
         <Feather name="trash-2" color="#ff3f4b" size={25} />
       </TouchableOpacity>
     </View>
@@ -32,5 +37,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     flexDirection: "row",
+    marginBottom: 12,
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    borderRadius: 4,
+    borderWidth: 0.3,
+    borderColor: "#8a8a8a",
+  },
+  item: {
+    color: "#fff",
   },
 });
